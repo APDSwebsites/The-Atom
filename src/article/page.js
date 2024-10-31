de5a5deff4f287eacae3893dbf327a74655fb1d3
peterjmassey <161041054+peterjmassey@users.noreply.github.com>
2024-10-31 15:43:17 -0500

    Create page.js

---
+++ src/article/page.js
@@ -0,0 +1,18 @@
+"use client";
+// pages/index.js
+// app/page.js
+import Layout from "../../components/Layout";
+//const fetch = require('node-fetch');
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
+export default function articlePage() {
+
+
+    return(
+
+        <Layout>
+
+        </Layout>
+    )
+}
